26abfa6b699366e2af3c1f3a877108141fea8984
Mannan <mshukla@MrLeeM1.local>
2022-03-01 13:59:48 -0500

    api done

--- api.py
+++ api.py
@@ -3,33 +3,35 @@ from flask import request
 import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from flask_pymongo import PyMongo
 from bson.json_util import *
 
 app = flask.Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://admin:beckrekekre@cluster0.uc4dj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 app.config["DEBUG"] = True
 
 client = MongoClient("mongodb+srv://admin:beckrekekre@cluster0.uc4dj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", server_api=ServerApi('1'))
-
-mongo = PyMongo(app)
 
 db = client.weather
 collection = db.data
 
 @app.route('/', methods=['GET'])
 def home():
-    return "welcome home, big bro!"
+    return "refer to hedgedoc for api path"
+
+@app.errorhandler(404)
+def not_found(e):
+    return "refer to hedgedoc for api usage"
 
 @app.route('/api/data')
 def page():
-    station = request.args['station']
-    time = request.args['time']
-    filters = request.args['filters']
-    results = list(collection.find({"station":station}))
+    station = int(request.args['station'])
+    time = int(request.args['time'])
 
-    return str(loads(dumps(list(collection.find({"station":0})))))
+    results = []
 
-    #return f"station name is {station}, the time is {time}, and the filters added are {filters}"
+    for result in list(collection.find({"station":station})):
+        if result["time"] >= time:
+            results.append(result)
+    
+    return str(loads(dumps(results)))
 
 app.run()
